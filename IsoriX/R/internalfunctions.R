9212d7a5695a249237f3fa123020bdb5bacc03af
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-04-21 18:00:53 +0200

    new function .info_package

--- IsoriX/R/internalfunctions.R
+++ IsoriX/R/internalfunctions.R
@@ -187,3 +187,24 @@
   }
   stop("'var' has an unknown class")
 }
+
+
+#' Provide some basic information about the code of the package
+#'
+#' This function provides the number of functions and lines of code of the
+#' package. The results depend on whether the function is called from within the
+#' compiled or the raw package. When called from the compiled package, the
+#' number of functions only gives the number of exported functions, and the
+#' number of lines of code no longer includes documentation and examples. This
+#' function is for development purpose only and is thus not exported.
+#'
+.info_package <- function() {
+  print(paste("number of functions =", length(ls("package:IsoriX"))))
+  if (requireNamespace("R.utils", quietly = TRUE)) {
+    files <- dir(paste0(system.file(package = "IsoriX"), "/R/"))
+    filenames_R <- paste0(system.file(package = "IsoriX"), "/R/", files)
+    lines_code <- sum(sapply(filenames_R, function(file) R.utils::countLines(file)))
+    print(paste("number of lines of code =", lines_code))
+  } else {message("Install the package R.utils for more info.")}
+  return(invisible(NULL))
+}
